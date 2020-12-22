b88f0ef1bba5966f639f61e6836233ed421226cb
diy-z <t19863751130@gmail.com>
2020-12-22 23:53:11 +0800

    修改了User、UserRepoisitory、UserRepositotyTest类的部分代码

--- src/test/java/com/androidweb/repository/UserRepositoryTest.java
+++ src/test/java/com/androidweb/repository/UserRepositoryTest.java
@@ -1,8 +1,11 @@
 package com.androidweb.repository;
 
+import com.androidweb.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +18,21 @@ class UserRepositoryTest {
     @Test
     void findAll(){
         System.out.println(userRepository.findAll());
+    }
+
+    @Test
+    void addUser(){
+        User user = new User();
+        user.setId(9);
+        user.setName("Zhang");
+        user.setPassword("123123");
+        userRepository.save(user);
+    }
+
+    @Test
+    void findUserById(){
+        Optional<User> byId = userRepository.findById(1);
+        System.out.println("byId -- > " + byId);
     }
 
 }
