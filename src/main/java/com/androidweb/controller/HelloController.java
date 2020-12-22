38f5f3214788dd9ad6725224f18bc87ccd9caa88
Yu <2706289018@qq.com>
2020-12-22 17:43:01 +0800

    CI 测试 - src

--- src/main/java/com/androidweb/controller/HelloController.java
+++ src/main/java/com/androidweb/controller/HelloController.java
@@ -3,7 +3,7 @@ package com.androidweb.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**/
+
 
 @RestController
 public class HelloController {
