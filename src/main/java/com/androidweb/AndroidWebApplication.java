e2d176c5add8c713298109a56a9757bea9d811a1
Yu <2706289018@qq.com>
2020-12-22 15:29:59 +0800

    Initial commit

---
+++ src/main/java/com/androidweb/AndroidWebApplication.java
@@ -0,0 +1,13 @@
+package com.androidweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroidWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/androidweb/AndroidWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.androidweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndroidWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
