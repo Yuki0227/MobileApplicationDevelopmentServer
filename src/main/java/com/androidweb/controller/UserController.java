dbe78319ee3d7ae2ef385dd16d42b5f5cbd7eba4
Yu <2706289018@qq.com>
2020-12-24 23:33:49 +0800

    优化导入

--- src/main/java/com/androidweb/controller/UserController.java
+++ src/main/java/com/androidweb/controller/UserController.java
@@ -5,9 +5,6 @@ import com.androidweb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.util.List;
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
