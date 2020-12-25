1b8afce15315afe8e0b948a85a6a82732dbc6e46
diy-z <t19863751130@gmail.com>
2020-12-25 09:37:59 +0800

    对TaskAssignController类补充了一些方法,增加了按创建者id号查询其创建的所有任务、修改任务的功能,均通过了postman接口测试

--- src/main/java/com/androidweb/controller/TaskAssignController.java
+++ src/main/java/com/androidweb/controller/TaskAssignController.java
@@ -5,6 +5,8 @@ import com.androidweb.repository.TaskAssignRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/taskAssign")
 public class TaskAssignController {
@@ -14,12 +16,32 @@ public class TaskAssignController {
     /**
      * 注意从前端传参数的时候,可以使用传键-值对的方式，不过需要注意传的时候键的名字要和TaskAssign中对应的参数名一致,值需要String类型的
      * @param task 根据传入的参数自动封装成TaskAssign类
+     * @return 返回添加后的对象
      */
     @PostMapping("/add")
-    public void add(@RequestBody TaskAssign task){
-        assignRepository.save(task);
+    public TaskAssign add(@RequestBody TaskAssign task){
+        return assignRepository.save(task);
     }
 
+    /**
+     * 注意在从前端传参数时，注意要注明任务的id号
+     * @param task 根据传入的参数自动封装成TaskAssign类
+     * @return 返回修改后的对象
+     */
+    @PostMapping("/update")
+    public TaskAssign update(@RequestBody TaskAssign task){
+        return assignRepository.save(task);
+    }
+
+    /**
+     * 根据创建者的id返回他所创建的所有任务
+     * @param creatorId 创建任务的那个用户的id
+     * @return  返回创建者对应的所有任务
+     */
+    @PostMapping("/findAll")
+    public List<TaskAssign> findTaskAssignByCreatorId(@RequestParam Integer creatorId){
+        return assignRepository.findAllByCreatorId(creatorId);
+    }
 
 
 
--- src/main/java/com/androidweb/repository/TaskAssignRepository.java
+++ src/main/java/com/androidweb/repository/TaskAssignRepository.java
@@ -2,10 +2,19 @@ package com.androidweb.repository;
 
 import com.androidweb.entity.TaskAssign;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TaskAssignRepository extends JpaRepository<TaskAssign,Integer> {
 
 
+    /**
+     * 根据创建者的creatorId来返回它创建的任务
+     * @return 返回创建者对应的所有任务
+     */
+    @Query(value = "from TaskAssign where creatorId = ?1")
+    List<TaskAssign> findAllByCreatorId(Integer creatorId);
 }
