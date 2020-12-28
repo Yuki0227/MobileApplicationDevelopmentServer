1b6a9c3ee0948ef0e9d03a85668d22d0357325e8
Yu <2706289018@qq.com>
2020-12-28 08:03:28 +0800

    更新数据库字段名称

--- src/main/java/com/androidweb/entity/question/JudgmentQuestion.java
+++ src/main/java/com/androidweb/entity/question/JudgmentQuestion.java
@@ -4,15 +4,18 @@ package com.androidweb.entity.question;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class JudgmentQuestion {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String question;
     private boolean answer;
     private String parse;
-
+    private String type;
 }
--- src/main/java/com/androidweb/entity/question/ProgrammingQuestion.java
+++ src/main/java/com/androidweb/entity/question/ProgrammingQuestion.java
@@ -4,16 +4,20 @@ package com.androidweb.entity.question;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
 public class ProgrammingQuestion {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String question;
     private String input;
     private String output;
     private String sampleInput;
     private String sampleOutput;
+    private String type;
 }
